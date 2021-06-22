7be2f0e06f267c3e4c5d8d0034cdad5c37d29fab
lpk92 <75961728+lpk92@users.noreply.github.com>
2021-06-22 08:48:19 +0800

    Update jd_jdfactory.js

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -44,8 +44,8 @@ if ($.isNode()) {
 }
 let wantProduct = ``;//心仪商品名称
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-const inviteCodes = [`P04z54XCjVWnYaS5u2ak7ZCdan1Bdd2GGiWvC6_uERj`, 'P04z54XCjVWnYaS5m9cZ2ariXVJwHf0bgkG7Uo',
-`T0225KkcRB8c_VODck-nl_8IdgCjVWnYaS5kRrbA`];
+const inviteCodes = [`T016aHHXlqamI-ZV9JZgCjVWnYaS5kRrbA`, `T016anXulbWULOF19Y9NCjVWnYaS5kRrbA`,
+`T023_fx0RhYe8FLRJxr1lfMKcU8CjVWnYaS5kRrbA`,`T018v_h6SR0e8VfXIBmb1ACjVWnYaS5kRrbA`];
 !(async () => {
   await requireConfig();
   if (!cookiesArr[0]) {
