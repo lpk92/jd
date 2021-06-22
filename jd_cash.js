b7ba758cd27365f012d86cd007a192fd76a95f85
lpk92 <75961728+lpk92@users.noreply.github.com>
2021-06-22 08:54:10 +0800

    Update jd_cash.js

--- jd_cash.js
+++ jd_cash.js
@@ -31,8 +31,8 @@ let helpAuthor = true;
 const randomCount = $.isNode() ? 5 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `eU9YL5XqGLxSmRSAkwxR@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA@eU9YMZ_gPpRurC-foglg@eU9Ya77gZK5z-TqHn3UWhQ@eU9Yaui2ZP4gpG-Gz3EThA@eU9YaeizbvQnpG_SznIS0w`,
-  `-4msulYas0O2JsRhE-2TA5XZmBQ@eU9Yar_mb_9z92_WmXNG0w@eU9YaO7jMvwh-W_VzyUX0Q@eU9YaurkY69zoj3UniVAgg@eU9YaOnjYK4j-GvWmXIWhA@eU9YaO23bvtyozuGyHsR1A`
+  `9ZeTulQNsE6kJeJB@95OquUc_v0mEJPts@YBowauS1Y_og9m7UyncT02A@9bWbuVo5s3yZJPB4zXIb@9bWbuVo5sFqmJOdRHuae@Ih4-Ze-1Yv8m8W26iw`,
+  `9ZeTulQNsE6kJeJB@95OquUc_v0mEJPts@YBowauS1Y_og9m7UyncT02A@9bWbuVo5s3yZJPB4zXIb@9bWbuVo5sFqmJOdRHuae@Ih4-Ze-1Yv8m8W26iw`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
